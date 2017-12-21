b42376b2b4f79e38f5980c809643600a9b9f2c75
Frank Paulo Filho <me@frankpf.com>
2017-12-21 17:17:34 -0200

    'Refined' class types now work, add .where() implementation
    As an example, after calling the method `select()`, we refine the type of
    Table to remove `select()` from the available methods. This means that
    the compiler ensures that `select()` won't ever be called twice.

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,59 @@ export default function SafeQL<Schema>(uri: string) {
 	}
 }
 
-
-class Table<Schema, Name extends string, Fields extends keyof Schema> {
+class Table<Schema, Name extends string, Fields extends keyof Schema, DisabledKeys extends keyof Table<Schema, Name, Fields, DisabledKeys> = 'name'> {
 	private client: pg.Client
 
-	constructor(private name: Name, private uri: string, private selector?: string, private filter?: string) {
+	constructor(public readonly name: Name,
+		        private uri: string,
+		        private selector?: '*' | (keyof Schema)[],
+		        private filter?: Partial<{ [Field in keyof Schema]: Schema[Field] }>
+		        ) {
 		this.client = new pg.Client({ connectionString: uri })
 	}
-	select                                  (items: '*'                ): TableWithoutSelect<Schema, Name, keyof Schema>
-	select<FieldSubset extends keyof Schema>(items: FieldSubset[]      ): TableWithoutSelect<Schema, Name, FieldSubset>
-	select<FieldSubset extends keyof Schema>(items: '*' | FieldSubset[]): ( TableWithoutSelect<Schema, Name, keyof Schema> | TableWithoutSelect<Schema, Name, FieldSubset>)
-	{
+
+	select                                  (items: '*'              ): DisableSelect<Schema, Name, keyof Schema, DisabledKeys>
+	select<FieldSubset extends keyof Schema>(items: FieldSubset[]    ): DisableSelect<Schema, Name, FieldSubset, DisabledKeys>
+	select<FieldSubset extends keyof Schema>(items: '*' | FieldSubset[]): DisableSelect<Schema, Name, keyof Schema, DisabledKeys> | DisableSelect<Schema, Name, FieldSubset, DisabledKeys> {
 		if (items == '*') {
 			const selector = '*'
-			return new Table<Schema, Name, keyof Schema>(this.name, this.uri, selector)
+			return new Table<Schema, Name, keyof Schema, DisabledKeys>(this.name, this.uri, selector) as any
 		} else {
-			const selector = `${items}`
-			return new Table<Schema, Name, keyof Schema>(this.name, this.uri, selector)
+			const selector = items
+			return new Table<Schema, Name, FieldSubset, DisabledKeys>(this.name, this.uri, selector) as any
 		}
 	}
 
-	where(obj: Partial<{ [Field in Fields]: Schema[Field] }>) {
+	where(obj: Partial<{ [Field in keyof Schema]: Schema[Field] }>): DisableWhere<Schema, Name, Fields, DisabledKeys> {
 		/* unimplemented */
+		return new Table<Schema, Name, Fields, DisabledKeys>(this.name, this.uri, this.selector, obj) as any
 	}
 
-	exec(): Promise< Pick<Schema, Fields>[] > {
+	exec(): Promise< Pick<Schema, Fields>[]> {
+		this.client.connect()
+		if (!this.selector) {
+			throw new Error('.select() not called')
+		}
+
+		let selectStr;
+		if (this.selector == '*') {
+			selectStr = '*'
+		} else {
+			selectStr = this.selector!.join(', ')
+		}
+
+		if (this.filter) {
+			let whereStr = ''
+			for (const [key, val] of Object.entries(this.filter))  {
+				whereStr += `${key}='${val}' AND`
+			}
+			// Remove trailing AND
+			whereStr = whereStr.slice(0, whereStr.length - 4)
+			return this.client
+				.query(`SELECT ${selectStr} FROM ${this.name} WHERE ${whereStr}`)
+				.then(result => result.rows)
+		}
+
 		return this.client
 			.query(`SELECT ${this.selector} FROM ${this.name}`)
 			.then(result => result.rows)
