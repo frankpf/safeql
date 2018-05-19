23688682ce1a998b9f92d6bfc65161e781d6433b
Frank Paulo Filho <me@frankpf.com>
2018-05-19 12:57:23 -0300

    Experiment with where

--- src/index.ts
+++ src/index.ts
@@ -3,43 +3,60 @@
 import * as pg from 'pg'
 import { users, products, prices_history } from './osm'
 import { Omit } from 'type-zoo'
+import * as GetmoreSchema from './GetmoreSchema'
 
-export default function SafeQL<Schema>(uri: string) {
-	return function buildTable<Name extends keyof Schema>(name: Name) {
-		return new Table<Schema[Name], Name, keyof Schema[Name]>(name, uri)
+type DatabaseLike<D> = {
+	[K in keyof D]: SchemaLike<D[K]>
+}
+export default function SafeQL<Database extends DatabaseLike<Database>>(uri: string) {
+	return function buildTable<Name extends keyof Database>(name: Name) {
+		return new Table<Database[Name], Name, keyof Database[Name], false>(name, uri)
 	}
 }
 
-class Table<Schema, Name extends string, Fields extends keyof Schema, DisabledKeys extends keyof Table<Schema, Name, Fields, DisabledKeys> = 'name'> {
+class Table<Schema extends SchemaLike<Schema>, Name extends string, Columns extends keyof Schema, IsPrimary extends boolean> {
 	private client: pg.Client
 
 	constructor(
-	           public readonly name: Name,
-	           private uri: string,
-	           private selector?: '*' | (keyof Schema)[],
-	           private filter?: Partial<{ [Field in keyof Schema]: Schema[Field] }>
-	           ){
+		public readonly name: Name,
+		private uri: string,
+		private selector?: '*' | (keyof Schema)[],
+		private filter?: { [col: string]: any }
+	) {
 		//this.client = pgPromise()(uri)
 		this.client = new pg.Client({ connectionString: uri })
 	}
 
-	select                                  (items: '*'                ): DisableSelect<Schema, Name, keyof Schema, DisabledKeys>
-	select<FieldSubset extends keyof Schema>(items: FieldSubset[]      ): DisableSelect<Schema, Name, FieldSubset, DisabledKeys>
-	select<FieldSubset extends keyof Schema>(items: '*' | FieldSubset[]): DisableSelect<Schema, Name, keyof Schema, DisabledKeys> | DisableSelect<Schema, Name, FieldSubset, DisabledKeys> {
+	/*
+	select                                  (items: '*'                ): Table<Schema, Name, keyof Schema>
+	select<FieldSubset extends keyof Schema>(...items: FieldSubset[]      ): Table<Schema, Name, FieldSubset>
+	select<FieldSubset extends keyof Schema>(...items: '*' | FieldSubset[]): Table<Schema, Name, keyof Schema> | Table<Schema, Name, FieldSubset> {
 		if (items == '*') {
 			const selector = '*'
-			return new Table<Schema, Name, keyof Schema, DisabledKeys>(this.name, this.uri, selector) as any
+			return new Table<Schema, Name, keyof Schema>(this.name, this.uri, selector) as any
 		} else {
 			const selector = items
-			return new Table<Schema, Name, FieldSubset, DisabledKeys>(this.name, this.uri, selector) as any
+			return new Table<Schema, Name, FieldSubset>(this.name, this.uri, selector) as any
 		}
 	}
+	*/
 
-	where(obj: Partial<{ [Field in keyof Schema]: Schema[Field] }>): DisableWhere<Schema, Name, Fields, DisabledKeys> {
-		return new Table<Schema, Name, Fields, DisabledKeys>(this.name, this.uri, this.selector, obj) as any
+	select<SelectedColumns extends keyof Schema>(...items: SelectedColumns[]): Table<Schema, Name, SelectedColumns, IsPrimary> {
+		const selector = items
+		return new Table<Schema, Name, SelectedColumns, IsPrimary>(this.name, this.uri, selector) as any
 	}
 
-	exec(): Promise< Pick<Schema, Fields>[]> {
+	where<
+		WhereColumn extends keyof Schema,
+		Primaries = { [K in WhereColumn]: Schema[K]['primaryKey'] }
+	>(obj: Partial<{ [K in WhereColumn]: Schema[K]['type'] }>): Table<Schema, Name, Columns, ValuesOf<Primaries> extends false ? false : true> {
+		return new Table<Schema, Name, Columns, ValuesOf<Primaries> extends false ? false : true>(this.name, this.uri, this.selector, obj) as any
+	}
+
+	execM<Prim extends boolean = IsPrimary extends false ? false : true>(x: Prim ) {}
+	exec<
+		Picked extends Pick<Schema, Columns> = Pick<Schema, Columns>, Out = { [K in keyof Picked]: Picked[K]['type'] }
+	>(): IsPrimary extends false ? Promise<Out[]> : Promise<Out> {
 		this.client.connect()
 		if (!this.selector) {
 			throw new Error('.select() not called')
@@ -70,5 +87,89 @@ class Table<Schema, Name extends string, Fields extends keyof Schema, DisabledKe
 	}
 }
 
-type DisableSelect<S,N extends string,T extends keyof S, R extends keyof Table<S,N,T,R>>=Omit<Table<S,N,T,R|'select'>, R|'select'>
-type DisableWhere<S,N extends string,T extends keyof S,R extends keyof Table<S,N,T,R>>=Omit<Table<S,N,T, R|'where'>, R|'where'>
+
+type Tk = { id: { type: string }, x: { type: string } }
+
+
+type SchemaLike<T> = {
+	[K in keyof T]: { type: any, primaryKey: boolean, unique: boolean }
+}
+function selectOne<T extends SchemaLike<T>, Fields extends keyof T>(...columns: Fields[]): { [K in keyof Pick<T, Fields>]: T[K]['type'] }[] {
+	return 1 as any
+}
+
+function where<
+	T extends SchemaLike<T>,
+	WhereColumn extends keyof Result =  Result,
+	Column extends keyof T = keyof T,
+	Result = { [ K in keyof Pick<T, Column>]: T[K]['type'] },
+	WhereResult = { [ K in keyof Pick<T, WhereColumn>]: T[K]['type'] },
+	Primaries = { [ K in keyof Pick<T, WhereColumn>]: T[K]['primaryKey'] },
+>(items: Result[], x: Partial<WhereResult>): ValuesOf<Primaries> extends false ? Result[] : Result  {
+	return 1 as any
+}
+
+
+type A = {
+	id:			{ primaryKey: false, type: string, unique: false },
+	name:		{ primaryKey: false, type: number, unique: false }
+}
+type B = {
+	id:			{ primaryKey: true, type: string, unique: false },
+	name:		{ primaryKey: false, type: number, unique: false }
+}
+
+let items = selectOne<B, 'id' | 'name'>('id')
+let filteredP = where<B, 'id' >(items, { id: 'okdsa' })
+let filteredNP = where<B, 'name' >(items, {name: 1, })
+
+type ValuesOf<T> = T[keyof T]
+
+type A1 = Assert<typeof items, {id: string}[]>
+type A2 = Assert<typeof filteredP, {id: string}>
+type A3 = Assert<typeof filteredNP, {id:string}[]>
+
+
+type Assert<T extends Assertion, Assertion> = true
+
+type MyDatabase = {
+	'users': users
+	'products': products
+}
+
+type GetmoreDatabase = {
+	'Users': GetmoreSchema.Users,
+	'Credits': GetmoreSchema.Credits
+}
+
+async function main() {
+const client = SafeQL<GetmoreDatabase>('')
+
+SafeQL<StaticTypes>('', RuntimeTypes)
+
+// TODO: Refactor where into whereOne and whereMany
+// Sadly, using just `where` wouldn't work. Even though
+// we know at compile-time if a primary key was passed in,
+// we can't know that at runtime (without resorting to something like io-ts).
+// However, what we CAN do is divide where into two functions:
+// - whereOne -> Errors if not given at least one UNIQUE key
+// - whereMany -> Errors if given at least one UNIQUE key
+let table =    client('Users')
+let selected = table.select('name', 'commonId')
+let filtered = selected.where({ id: 'string', commonId: 'string', phoneInfo:1 })
+
+const items = await filtered.execM(true)
+
+items.id
+items.deleted
+items.commonId
+items.name
+
+for (const item of items) {
+	let a = item.id
+	let b =  item.deleted
+	let c = item.commonId
+	let d = item.name
+}
+
+}
